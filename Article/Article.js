2e8335b000b10fb448fbd3ccc066b786bb84f9f3
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-15 16:09:02 +0100

    Animated the collapse and expand of the articles

--- Article/Article.js
+++ Article/Article.js
@@ -7,14 +7,38 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'Expand';
+    this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    this.attachCloseButton();
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    this.swapButtonLabel();
+  }
+
+  swapButtonLabel() {
+    const label = this.expandButton.textContent;
+    if (label === 'Click to Expand') {
+      this.expandButton.textContent = 'Click to Collapse';
+      return;
+    }
+
+    this.expandButton.textContent = 'Click to Expand';
+  }
+
+  attachCloseButton() {
+    const button = document.createElement('button');
+    button.textContent = 'X';
+    button.addEventListener('click', this.closeArticle.bind(this));
+
+    this.domElement.appendChild(button);
+  }
+
+  closeArticle() {
+    this.domElement.style.display = 'none';
   }
 }
 
