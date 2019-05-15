c0ff5c8b5dde43c202aedc22528acefdd1f3954b
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-15 19:04:46 +0100

    Added box shadow to the buttons and changed the collapse article button labes

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "Click to Expand";
+    this.expandButton.textContent = "Read more.";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", this.expandArticle.bind(this));
     this.attachCloseButton();
@@ -21,12 +21,12 @@ class Article {
 
   swapButtonLabel() {
     const label = this.expandButton.textContent;
-    if (label === "Click to Expand") {
-      this.expandButton.textContent = "Click to Collapse";
+    if (label === "Read more.") {
+      this.expandButton.textContent = "Hide text.";
       return;
     }
 
-    this.expandButton.textContent = "Click to Expand";
+    this.expandButton.textContent = "Read more.";
   }
 
   attachCloseButton() {
