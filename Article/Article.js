9962e0db5f3f76ac62b240bb6960ba060ad723dc
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-15 18:09:12 +0100

    Refactored the code base and moved the form styling to a new file

--- Article/Article.js
+++ Article/Article.js
@@ -4,41 +4,41 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = this.domElement.querySelector('.expandButton');
+    // create a reference to the ".expandButton" class.
+    this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'Click to Expand';
+    this.expandButton.textContent = "Click to Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    this.expandButton.addEventListener("click", this.expandArticle.bind(this));
     this.attachCloseButton();
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
+    this.domElement.classList.toggle("article-open");
     this.swapButtonLabel();
   }
 
   swapButtonLabel() {
     const label = this.expandButton.textContent;
-    if (label === 'Click to Expand') {
-      this.expandButton.textContent = 'Click to Collapse';
+    if (label === "Click to Expand") {
+      this.expandButton.textContent = "Click to Collapse";
       return;
     }
 
-    this.expandButton.textContent = 'Click to Expand';
+    this.expandButton.textContent = "Click to Expand";
   }
 
   attachCloseButton() {
-    const button = document.createElement('button');
-    button.textContent = 'X';
-    button.addEventListener('click', this.closeArticle.bind(this));
+    const button = document.createElement("button");
+    button.textContent = "X";
+    button.addEventListener("click", this.closeArticle.bind(this));
 
     this.domElement.appendChild(button);
   }
 
   closeArticle() {
-    this.domElement.style.display = 'none';
+    this.domElement.style.display = "none";
   }
 }
 
@@ -50,30 +50,30 @@ class Article {
 
 */
 
-const articles = document.querySelectorAll('.article');
+const articles = document.querySelectorAll(".article");
 articles.forEach(article => {
   new Article(article);
-})
+});
 
-// fetch form data
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
+// fetch form data and populate article when form is submitted
+const form = document.querySelector("form");
+form.addEventListener("submit", event => {
   event.preventDefault();
-  const title = document.querySelector('input').value;
-  const body = document.querySelector('textarea').value;
+  const title = document.querySelector("input").value;
+  const body = document.querySelector("textarea").value;
 
   new CreateArticles({
     title,
     body
   });
-})
+});
 
 class CreateArticles {
   constructor(newsItem) {
     this.title = newsItem.title;
     this.body = newsItem.body;
     this.date = Date().slice(0, 16);
-    this.article = document.createElement('div');
+    this.article = document.createElement("div");
 
     this.insertH2();
     this.insertDate();
@@ -85,32 +85,32 @@ class CreateArticles {
   }
 
   insertH2() {
-    const header = document.createElement('h2');
+    const header = document.createElement("h2");
     header.textContent = this.title;
     this.article.appendChild(header);
   }
 
   insertDate() {
-    const paragraph = document.createElement('p');
+    const paragraph = document.createElement("p");
     paragraph.textContent = this.date;
-    paragraph.setAttribute('class', 'date');
+    paragraph.setAttribute("class", "date");
     this.article.appendChild(paragraph);
   }
 
   insertBody() {
-    const body = document.createElement('p');
+    const body = document.createElement("p");
     body.textContent = this.body;
     this.article.appendChild(body);
   }
 
   addCloseBody() {
-    const closeBody = document.createElement('span');
-    closeBody.setAttribute('class', 'expandButton');
+    const closeBody = document.createElement("span");
+    closeBody.setAttribute("class", "expandButton");
     this.article.appendChild(closeBody);
   }
 
   appendArticle() {
-    this.article.setAttribute('class', 'article');
-    document.querySelector('.articles').prepend(this.article);
+    this.article.setAttribute("class", "article");
+    document.querySelector(".articles").prepend(this.article);
   }
 }
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,28 +1,19 @@
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-  // if (menu.classList.contains("menu--open")) {
-  //   menu.classList.toggle("menu--open");
-  //   menu.classList.add("slide-out");
-  // }
-  menu.style.display = 'block';
-
+  // Toggle the "menu--open" class on your menu reference.
   menu.classList.toggle("menu--open");
-  // menu.classList.toggle('slide-out')
-  // menu.classList.toggle('menu')
-
-  // page.addEventListener('click', toggleMenu);
-}
+};
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector('div.menu');
-menu.style.display = 'none';
+const menu = document.querySelector("div.menu");
+
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelector('.menu-button');
+const menuButton = document.querySelector(".menu-button");
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', toggleMenu);
+menuButton.addEventListener("click", toggleMenu);
 
 // close the menu when the user clicks anywhere on the page
-const page = document.querySelector('.articles');
-page.addEventListener('click', (e) => {
+const page = document.querySelector(".articles");
+page.addEventListener("click", () => {
   menu.classList.remove("menu--open");
 });
